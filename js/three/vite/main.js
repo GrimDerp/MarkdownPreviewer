ebb7e77159b3076b457df2ac4e5537cddf45170f
BC <admin@superzen.org>
2023-04-09 13:16:36 -0600

    Update main.js

--- js/three/vite/main.js
+++ js/three/vite/main.js
@@ -1,59 +1,47 @@
-//howdy yall
-
+// howdy yall
 //@ts-check
-
-/*import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-*/
-
-/*document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-*/
-
-//setupCounter(document.querySelector('#counter'))
-
-//import * as THREE from 'https://unpkg.com/three@0.151.3/build/three.module.js'
-// "three/addons/": "https://unpkg.com/three@<0.151.3>/examples/jsm/"
-
-import * as dat from 'dat.gui'
-console.log(dat)
-//node
+import gsap from 'gsap'
+import * as dat from 'dat.gui';
 import * as THREE from 'three';
 import { Light } from 'three';
 //says its broken but seems to work, i dunno
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+//need three scene, then world
+const scene = new THREE.Scene()
+
+//cameras & casters
+const raycaster = new THREE.Raycaster()
+
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  innerWidth / innerHeight,
+  0.1,
+  1000
+  )
+
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+renderer.render(scene, camera)
+
+new OrbitControls(camera, renderer.domElement)
+camera.position.z = 5
 
 const gui = new dat.GUI()
 const world = {
   plane: {
-    width: 10,
-    height: 10,
-    widthSegments: 10,
-    heightSegments: 10
+    width: 19,
+    height: 19,
+    widthSegments: 17,
+    heightSegments: 17
   }
 }
-console.log(gui)
 
-
+//gui action?
 // width gui
 gui.add(world.plane, 'width', 1, 64, ).onChange(generatePlane)
  //height gui 
@@ -63,7 +51,7 @@ gui.add(world.plane, 'widthSegments', 1, 128, ).onChange(generatePlane)
 gui.add(world.plane, 'heightSegments', 1, 128, ).onChange(generatePlane)  
 
 
-
+//action?
 function generatePlane(){
   planeMesh.geometry.dispose()
   planeMesh.geometry = new THREE.PlaneGeometry(
@@ -72,131 +60,145 @@ function generatePlane(){
     world.plane.widthSegments, 
     world.plane.heightSegments
     )
-
-const {array} = planeMesh.geometry.attributes.position
-  for (let i = 0; i < array.length; i +=3) {
-  const x = array[i]
-  const y = array[i + 1]
-  const z = array[i + 2]
-
-  array[i + 2] = z + Math.random()
-  //console.log(array[i])
-  }
-  console.log(world.plane.height);
 }
 
-
-
-
-//const controls = new OrbitControls( camera, renderer.domElement );
-//const loader = new GLTFLoader();
-
-
-//cameras & casters
-const raycaster = new THREE.Raycaster()
-//console.log(raycaster)
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
-  75, 
-  innerWidth / innerHeight,
-  0.1,
-   1000
-   )
-const renderer = new THREE.WebGLRenderer(
-    //gonna put something here at some point
-   )
-
-/*console.log(scene)
-console.log(camera)
-console.log(renderer)
-console.log(boxGeometry)
-console.log(material)
-console.log(mesh)
-*/
-
-renderer.setSize(innerWidth, innerHeight)
-renderer.setPixelRatio(devicePixelRatio)
-document.body.appendChild(renderer.domElement)
-
-/*
- // mesh.rotation.x += 0.031987 * Math.random() +.02
- // mesh.rotation.y += 0.0279 * Math.random()
- // mesh.position.z += 0.0019 * Math.random()
-  //planeMesh.rotation.x += .00279 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
-const mesh = new THREE.Mesh(boxGeometry, material)
-scene.add(mesh)
-*/
-new OrbitControls(camera, renderer.domElement)
-camera.position.z = 5
-
-const planeGeometry = new THREE.PlaneGeometry(15, 15, 30, 30)
+const planeGeometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, world.plane.widthSegments, world.plane.heightSegments, 19, 19, 17, 17)
 const planeMaterial = new THREE.MeshPhongMaterial({
-    color: 0xff0000,
+    //color: 0xff0000,
     side: THREE.DoubleSide,
-    flatShading: true  //THREE.FlatShading
+    flatShading: true,  //THREE.FlatShading
+    vertexColors: true
   })
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
 scene.add(planeMesh)
-console.log(planeGeometry.attributes.position)
 
+//vertice position randomization
+const {array} = planeMesh.geometry.attributes.position
+const randomValues = []
+for (let i = 0; i < array.length; i ++) {
+  if (i % 3 === 0) {
+  const x = array[i]
+  const y = array[i + 1]
+  const z = array[i + 2]
+  
+  array[i] = x + (Math.random() -0.5)
+  array[i + 1] = y + (Math.random() -0.5)
+  array[i + 2] = z + Math.random()
+  }
+  randomValues.push(Math.random() - 0.5)
+} 
+
+planeMesh.geometry.attributes.position.randomValues = randomValues
+planeMesh.geometry.attributes.position.originalPosition =
+planeMesh.geometry.attributes.position.array
+
+//color attribute addition
 const colors = []
 for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++){
-  colors.push(1, 1, 0)
+  colors.push(0, .19, 0.4)
 }
-console.log(colors)
-
 planeMesh.geometry.setAttribute(
   'color', 
   new THREE.BufferAttribute(new Float32Array(colors), 3)
-  )
-
-console.log(planeMesh.geometry.attributes)
+)
 //lighting
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0, 0, 1)
 scene.add(light)
-//------------------
+
 const backLight = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0, 0, -1)
 scene.add(backLight)
-//console.log(planeMesh.geometry.attributes.position.array)
-/* tucked into the generatePlane Fx!!
-const {array} = planeMesh.geometry.attributes.position
-//gonna adjust the plane's vertices; array length is 363
-for (let i = 0; i < array.length; i +=3) {
-  const x = array[i]
-  const y = array[i + 1]
-  const z = array[i + 2]
-  array[i + 2] = z + Math.random()
-  //console.log(array[i])
-}
-console.log(planeMesh.geometry.attributes);
-*/
+
 const mouse = {
   x: undefined,
   y: undefined
 }
 
-
+let frame = 0
 function animate(){
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
+requestAnimationFrame(animate)
+renderer.render(scene, camera)
+//animation loop
+raycaster.setFromCamera(mouse, camera)
+frame += 0.01
 
-  raycaster.setFromCamera(mouse, camera)
-  const intersects = raycaster.intersectObject(planeMesh)
-  if (intersects.length > 0) {
-    //console.log('intersecting')
-   // console.log(intersects[0].face).intersects[0].object.geometry.attributes.color.setX(0, 0)
-    intersects[0].object.geometry.attributes.color.setX(intersects[0].face.a, 0)
-    //needsUpdate = true
-    intersects[0].object.geometry.attributes.color.needsUpdate = true
+const { 
+  array,
+  originalPosition,
+  randomValues 
+} = planeMesh.geometry.attributes.position
+for (let i = 0; i < array.length; i+=3) {
+  const element = array[i];
+  // x
+  array[i] = originalPosition[i] + 
+    Math.cos(frame + randomValues[i]) * 0.003
+// y
+  array[i + 1] = originalPosition[i +1] + 
+    Math.sin(frame + randomValues[i + 1]) * 0.003
+}
+planeMesh.geometry.attributes.position.needsUpdate = true
+
+const intersects = raycaster.intersectObject(planeMesh)
+if (intersects.length > 0) {
+  const {color} = intersects[0].object.geometry.attributes
+
+    //vertice 1
+    color.setX(intersects[0].face.a, 0.1)
+    color.setY(intersects[0].face.a, 0.5)
+    color.setZ(intersects[0].face.a, 1)
+    //vertice 2
+    color.setX(intersects[0].face.b, 0.1)
+    color.setY(intersects[0].face.b, 0.5)
+    color.setZ(intersects[0].face.b, 1)
+      //vertice 3
+    color.setX(intersects[0].face.c, 0.1)
+    color.setY(intersects[0].face.c, 0.5)
+    color.setZ(intersects[0].face.c, 1)
+  //needsUpdate = true
+  intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+
+
+
+
+
+    const initialColor = {
+      r: 0,
+      g: 0.19,
+      b: 0.4
+    }
+
+    const hoverColor = {
+      r: 0.1,
+      g: 0.5,
+      b: 1
+    }
+
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      onUpdate: () => {
+        //vertice 1
+      color.setX(intersects[0].face.a, hoverColor.r)
+      color.setY(intersects[0].face.a, hoverColor.g)
+      color.setZ(intersects[0].face.a, hoverColor.b)
+      //vertice 2
+      color.setX(intersects[0].face.b, hoverColor.r)
+      color.setY(intersects[0].face.b, hoverColor.g)
+      color.setZ(intersects[0].face.b, hoverColor.b)
+        //vertice 3
+      color.setX(intersects[0].face.c, hoverColor.r)
+      color.setY(intersects[0].face.c, hoverColor.g)
+      color.setZ(intersects[0].face.c, hoverColor.b)
+      color.needsUpdate = true
+      }
+    })
   }
 }
 
-//[0, 0, 1, 0, 0, 1, ... 0, 0]
-//movin stuff around
+
 animate()
 
 
@@ -205,9 +207,8 @@ animate()
 addEventListener('mousemove', (event) => 
 {
   mouse.x = (event.clientX / innerWidth) * 2 - 1
-  mouse.x = (event.clientY / innerHeight) * 2 + 1
+  mouse.y = (event.clientY / innerHeight) * 2 + 1
   //console.log(event.clientY)
   //console.log(mouse)
 })
 
-renderer.render(scene, camera)
